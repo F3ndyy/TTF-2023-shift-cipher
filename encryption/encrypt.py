0ec5472700bf5692283841878509d881b5728f89
F3ndyy <matte.ratti@gmail.com>
2023-02-09 12:42:46 +0100

    RWARG

--- encryption/encrypt.py
+++ encryption/encrypt.py
@@ -3,10 +3,7 @@ import os
 
 
 def encrypt(text: str, key: int) -> str:
-    """Applies a shift cipher (also known as Caesar cipher) to the input text,
-    by rotating it LEFT of key number of positions.
-    Returns the rotated text.
-    """
+
     pass
 
 
@@ -16,11 +13,11 @@ def _main():
     k = os.getenv("CIPHER_KEY")
     print(type(k))
     # read from encrypt_input.txt
-    with open("./encryption/encrypt_input.txt") as input:
+    with open("./encrypt_input.txt") as input:
         print(input.readlines())
     # call encrypt on each line with your key
     # write the encrypted lines to encrypt_output.txtc
-    with open("./encryption/encrypt_output.txt", "w") as output:
+    with open("./encrypt_output.txt", "w") as output:
         output.write("TEST")
    
 
